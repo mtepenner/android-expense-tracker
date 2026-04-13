eb49a7b64170714616972630c377f3c79e85a88d
Matthew Timothy Erwin Penner <mtepenner@gmail.com>
2026-04-13 09:19:24 -0700

    Create Color.kt

---
+++ app/src/main/java/com/yourname/expensetracker/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.yourname.expensetracker.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Colors
+val md_theme_light_primary = Color(0xFF006C4C)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFF89F8C7)
+val md_theme_light_onPrimaryContainer = Color(0xFF002114)
+val md_theme_light_background = Color(0xFFFBFDF9)
+val md_theme_light_surface = Color(0xFFFBFDF9)
+
+// Dark Theme Colors
+val md_theme_dark_primary = Color(0xFF6CDBAC)
+val md_theme_dark_onPrimary = Color(0xFF003826)
+val md_theme_dark_primaryContainer = Color(0xFF005138)
+val md_theme_dark_onPrimaryContainer = Color(0xFF89F8C7)
+val md_theme_dark_background = Color(0xFF191C1A)
+val md_theme_dark_surface = Color(0xFF191C1A)
